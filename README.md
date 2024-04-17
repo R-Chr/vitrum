6931d9d268f2794820fd4b247b0d9717b4b3ee27
R-Chr <rasmusc@bio.aau.dk>
2024-04-17 13:43:40 -0700

    More work

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-# vitrum
+# ⏳ vitrum⏳ 
 
 Implementation of various analysis methods commonly used for disordered and glassy material structures.
 
 Package is built as an extension to the ASE python package
 
+
+## 🎯 Scope and functionality
 Working implementations of following analysis methods
 
 | Analysis method | Class method |
@@ -13,5 +15,17 @@ Working implementations of following analysis methods
 | Persistence diagram | .get_persistence_diagram() |
 | Local persistence | .get_local_persistence() |
 
-
+## 📖 Author
 Author: Rasmus Christensen (rasmusc@bio.aau.dk)
+
+## ⭐ Acknowledgements
+
+`vitrum` has been built with the help of several open-source packages.
+All of these are listed in setup.py.
+
+These packages include:
+ASE
+Numpy
+Pandas
+Dionysus
+Diode
--- functions.py
+++ functions.py
@@ -1,13 +1,13 @@
 import pandas as pd
-from ase import io
 import numpy as np
 from ase import Atoms
 import dionysus
 import diode
+from scipy.signal import argrelextrema
 
 
 class glass_Atoms(Atoms):
-    #def __init__(self):
+    # def __init__(self):
     #    super().__init__()
 
     def get_dist(self):
@@ -19,7 +19,7 @@ class glass_Atoms(Atoms):
         x_dif = np.where(x_dif > 0.5 * dim[0], np.abs(x_dif - dim[0]), x_dif)
         y_dif = np.where(y_dif > 0.5 * dim[1], np.abs(y_dif - dim[1]), y_dif)
         z_dif = np.where(z_dif > 0.5 * dim[2], np.abs(z_dif - dim[2]), z_dif)
-        i_i = np.sqrt(x_dif ** 2 + y_dif ** 2 + z_dif ** 2)
+        i_i = np.sqrt(x_dif**2 + y_dif**2 + z_dif**2)
         return i_i
 
     def get_pdf(self, target_atoms, rrange=10, nbin=100):
@@ -28,22 +28,25 @@ class glass_Atoms(Atoms):
         atom_1 = np.where(types == target_atoms[0])[0]
         atom_2 = np.where(types == target_atoms[1])[0]
         dist_list = distances[np.ix_(atom_1, atom_2)]
-        edges = np.linspace(0, rrange, nbin+1)
-        xval = edges[1:]-0.5*(rrange/nbin)
+        edges = np.linspace(0, rrange, nbin + 1)
+        xval = edges[1:] - 0.5 * (rrange / nbin)
         volbin = []
         for i in range(nbin):
-            vol = ((4/3)*np.pi*(edges[i+1])**3)-((4/3)*np.pi*(edges[i])**3)
+            vol = ((4 / 3) * np.pi * (edges[i + 1]) ** 3) - (
+                (4 / 3) * np.pi * (edges[i]) ** 3
+            )
             volbin.append(vol)
 
         h, bin_edges = np.histogram(dist_list, bins=nbin, range=(0, rrange))
         h[0] = 0
-        pdf = (h/volbin)/(dist_list.shape[0]*dist_list.shape[1]/self.get_volume())
+        pdf = (h / volbin) / (
+            dist_list.shape[0] * dist_list.shape[1] / self.get_volume()
+        )
         return xval, pdf
 
     def get_persistence_diagram(self, dimension=1, weights=None):
         coord = self.get_positions()
-        cell = self.get_cell()
-        data = np.column_stack([self.get_chemical_symbols(),coord])
+        data = np.column_stack([self.get_chemical_symbols(), coord])
         dfpoints = pd.DataFrame(data, columns=["Atom", "x", "y", "z"])
         chem_species = np.unique(self.get_chemical_symbols())
 
@@ -69,22 +72,122 @@ class glass_Atoms(Atoms):
         dgms = dionysus.init_diagrams(m, f)
 
         # Gather the PD of loop in a dataframe
-        dfPD = pd.DataFrame(data={"Birth": [p.birth for p in dgms[dimension]],
-                                  "Death": [p.death for p in dgms[dimension]],
-                                  })
+        dfPD = pd.DataFrame(
+            data={
+                "Birth": [p.birth for p in dgms[dimension]],
+                "Death": [p.death for p in dgms[dimension]],
+            }
+        )
         return dfPD
-    
-    def get_angular_dist(self, center_atom, neighbor_atoms):
-        print('Place_holder')
-    
+
+    def get_local_persistence(self, center_id, cutoff):
+        persistence_diagrams = []
+        centers = np.where(self.get_atomic_numbers() == center_id)[0]
+        for i in centers:
+            neighbors = np.where(self.get_dist()[i, :] < cutoff)[0]
+            neighborhood = self[neighbors]
+            neighborhood.center()
+            persistence_diagrams.append(neighborhood.get_persistence_diagram())
+        return persistence_diagrams
+
+    def get_angular_dist(self, center_type, neigh_type, cutoff="Auto"):
+        distances = self.get_dist()
+
+        types = self.get_atomic_numbers()
+        center_index = np.where(types == center_type)[0]
+        neigh_index = np.where(types == neigh_type)[0]
+
+        if cutoff == "Auto":
+            pdf = self.get_pdf(target_atoms=[center_type, neigh_type])
+            cutoff = pdf[0][self.find_min_after_peak(pdf[1])]
+        elif isinstance(cutoff, float | int):
+            cutoff = cutoff
+
+        angles = []
+
+        for center in center_index:
+            neighbors = np.where(
+                (distances[neigh_index, center] < cutoff)
+                & (distances[neigh_index, center] > 0)
+            )[0]
+            if neighbors.shape[0] < 2:
+                continue
+            upper_index = np.triu_indices(neighbors.shape[0], k=1)
+            comb_1 = np.meshgrid(neighbors, neighbors)[0][upper_index]
+            comb_2 = np.meshgrid(neighbors, neighbors)[1][upper_index]
+            indicies = np.vstack(
+                (neigh_index[comb_1], np.full(len(comb_1), center), neigh_index[comb_2])
+            ).T
+            angles.append(self.get_angles(indicies, mic=True))
+        return angles
+
+    def get_coordination_number(self, center_type, neigh_type, cutoff="Auto"):
+        distances = self.get_dist()
+        types = self.get_atomic_numbers()
+        atom_1 = np.where(types == center_type)[0]
+        atom_2 = np.where(types == neigh_type)[0]
+        dist_list = distances[np.ix_(atom_1, atom_2)]
+
+        if cutoff == "Auto":
+            pdf = self.get_pdf(target_atoms=[center_type, neigh_type])
+            cutoff = pdf[0][self.find_min_after_peak(pdf[1])]
+        elif isinstance(cutoff, float | int):
+            cutoff = cutoff
+        print(cutoff)
+        coordination_numbers = []
+        for center in range(len(atom_1)):
+            neighbors = np.where(
+                (dist_list[center, :] < cutoff) & (dist_list[center, :] > 0)
+            )[0]
+            coordination_numbers.append(neighbors.shape[0])
+        return coordination_numbers
+
+    def find_min_after_peak(self, padf):
+        mins = argrelextrema(padf, np.less_equal, order=4)[0]
+        second_min = [i for ind, i in enumerate(mins) if i != ind][0]
+        return second_min
+
+    def NBO_analysis(distances, center_index, neigh_index_1, neigh_index_2, cutoffs):
+        P_neigh_unique = []
+        for center in center_index:
+            neighbors = np.where(
+                (distances[neigh_index, center] < cutoff[0])
+                & (distances[neigh_index, center] > 0)
+            )[0]
+            P_neigh_unique.append([O_ind[neigh] for neigh in neighbors])
+        P_neigh_unique = np.unique(np.hstack(P_neigh_unique))
+
+        bond_order = [0, 0, 0, 0]
+        for neigh in P_neigh_unique:
+            neighbor_list_1 = np.where(
+                (distances[neigh_index_1, neigh] < cutoff[0])
+                & (distances[neigh_index_1, neigh] > 0)
+            )[0]
+            neighbor_list_2 = np.where(
+                (distances[neigh_index_2, neigh] < cutoff[1])
+                & (distances[neigh_index_2, neigh] > 0)
+            )[0]
+
+            if neighbor_list_P.shape[0] + neighbor_list_Fe.shape[0] == 1:
+                bond_order[0] += 1
+            elif neighbor_list_P.shape[0] + neighbor_list_Fe.shape[0] == 2:
+                bond_order[1] += 1
+            elif neighbor_list_P.shape[0] + neighbor_list_Fe.shape[0] == 3:
+                bond_order[2] += 1
+            elif neighbor_list_P.shape[0] + neighbor_list_Fe.shape[0] == 0:
+                bond_order[3] += 1
+        return coordination_number
 
 
-
-def get_msd():
-    print('Place_holder')
-
-
-
-
-xyz_start = io.read(f"/Volumes/My Passport for Mac/sodium silicate/30Na_800/data/1/propensity/1/md.lammpstrj", index=0 , format="lammps-dump-text")
-print(xyz_start.get_positions())
+def get_LAMMPS_dump_timesteps(filename):
+    with open(filename, encoding="utf-8") as f:
+        timesteps = []
+        lines = deque(f.readlines())
+        line = lines.popleft()
+        while len(lines) > 0:
+            if "ITEM: TIMESTEP" in line:
+                line = lines.popleft()
+                timesteps.append(int(line))
+            else:
+                line = lines.popleft()
+    return timesteps
